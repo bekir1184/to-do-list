a8d5ea009de3b23651f1088e43518a4c1a2d9d17
Bekir Ersever <bekirersever@outlook.com>
2021-01-07 23:10:48 +0300

    added local storage with JSON

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items')
 const itemsList = document.querySelector('.todo')
-const items = []
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
     e.preventDefault();
@@ -12,6 +12,7 @@ function addItem(e) {
 
     items.push(item)
     populateList(items, itemsList)
+    localStorage.setItem('items', JSON.stringify(items));
     this.reset()
 
 
@@ -27,5 +28,16 @@ function populateList(todos = [], todosList) {
       `;
     }).join('');
 }
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem)
+itemsList.addEventListener('click', toggleDone);
 populateList(items, itemsList);
